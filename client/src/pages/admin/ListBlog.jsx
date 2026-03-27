86ed524aeb010fc511a3ebc637e55d51445c1db6
Balogram-Code-Canvas <tunde.agboke@yahoo.com>
2026-03-27 16:33:53 +0100

    fix update

--- client/src/pages/admin/ListBlog.jsx
+++ client/src/pages/admin/ListBlog.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { blog_data } from '../../assets/assets';
 import BlogTableItem from '../../components/admin/BlogTableItem';
 import { useAppContext } from '../../context/AppContext';
+import { toast } from 'react-hot-toast'
 
 const ListBlog = () => {
 
